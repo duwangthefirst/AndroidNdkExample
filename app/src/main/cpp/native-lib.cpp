ded1b34b58b1f61fa2e0994c3c3ff18fe651a8b6
wangdu <2875138336@qq.com>
2021-05-27 13:43:01 +0800

    to be continued

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,45 +1,134 @@
 #include <jni.h>
 #include <string>
+#include <initializer_list>
+#include <memory>
+#include <cstdlib>
+#include <cstring>
+#include <jni.h>
+#include <cerrno>
+#include <cassert>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_dfanr_androidndkexample_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
+#include <EGL/egl.h>
+#include <GLES/gl.h>
 
-    #if defined(__arm__)
-        #if defined(__ARM_ARCH_7A__)
-            #if defined(__ARM_NEON__)
-                #if defined(__ARM_PCS_VFP)
-                    #define ABI "armeabi-v7a/NEON (hard-float)"
-                #else
-                    #define ABI "armeabi-v7a/NEON"
-                #endif
-            #else
-                #if defined(__ARM_PCS_VFP)
-                    #define ABI "armeabi-v7a (hard-float)"
-                #else
-                    #define ABI "armeabi-v7a"
-                #endif
-            #endif
-        #else
-            #define ABI "armeabi"
-        #endif
-    #elif defined(__i386__)
-        #define ABI "x86"
-    #elif defined(__x86_64__)
-        #define ABI "x86_64"
-    #elif defined(__mips64)  /* mips64el-* toolchain defines __mips__ too */
-        #define ABI "mips64"
-    #elif defined(__mips__)
-        #define ABI "mips"
-    #elif defined(__aarch64__)
-        #define ABI "arm64-v8a"
-    #else
-        #define ABI "unknown"
-    #endif
+#include <android/sensor.h>
+#include <android/log.h>
+#include <android_native_app_glue.h>
 
-    // c++支持类似`std::string hello = "str1" "str2" "str3";`的写法，等价于`std::string hello = "str1str2str3";`
-    // 宏定义ABI会被替换为"arm64-v8a"（包含双引号）
-    std::string hello = "Hello from C++, current ABI is " ABI ". ";
-    return env->NewStringUTF(hello.c_str());
+
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, "native-activity", __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "native-activity", __VA_ARGS__))
+
+struct saved_state {
+    float angle;
+    int32_t x;
+    int32_t y;
+};
+struct engine {
+    struct android_app* app;
+
+    ASensorManager* sensorManager;
+    const ASensor* accelerometerSensor;
+    ASensorEventQueue* sensorEventQueue;
+
+    int animating;
+    EGLDisplay display;
+    EGLSurface surface;
+    EGLContext context;
+    int32_t width;
+    int32_t height;
+    struct saved_state state;
+};
+
+static int engine_init_display(struct engine* engine) {
+    // 初始化OpenGL ES和EGL
+
+    EGLint w, h, format;
+
+    EGLConfig config = nullptr;
+    EGLSurface surface;
+    EGLContext context;
+
+
+    // 获取EGLDisplay对象
+    EGLDisplay eglDisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    // 初始化EGLDisplay对象
+    eglInitialize(eglDisplay, nullptr, nullptr);
+
+    /* Here, the application chooses the configuration it desires.
+     * find the best match if possible, otherwise use the very first one
+     */
+    const EGLint attribs[] = {
+            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+            EGL_BLUE_SIZE, 8,
+            EGL_GREEN_SIZE, 8,
+            EGL_RED_SIZE, 8,
+            EGL_NONE
+    };
+    EGLint numConfigs;
+    eglChooseConfig(eglDisplay, attribs, nullptr, 0, &numConfigs);
+    std::unique_ptr<EGLConfig[]> supportedConfigs(new EGLConfig[numConfigs]);
+    assert(supportedConfigs);
+    eglChooseConfig(eglDisplay, attribs, supportedConfigs.get(), numConfigs, &numConfigs);
+    assert(numConfigs);
+    auto i = 0;
+    for (; i < numConfigs; i++) {
+        auto& cfg = supportedConfigs[i];
+        EGLint r, g, b, d;
+        if (eglGetConfigAttrib(eglDisplay, cfg, EGL_RED_SIZE, &r) &&
+            eglGetConfigAttrib(eglDisplay, cfg, EGL_GREEN_SIZE, &g) &&
+            eglGetConfigAttrib(eglDisplay, cfg, EGL_BLUE_SIZE, &b) &&
+            eglGetConfigAttrib(eglDisplay, cfg, EGL_DEPTH_SIZE, &d) &&
+            r == 8 && g == 8 && b == 8 && d == 0 ) {
+
+            config = supportedConfigs[i];
+            break;
+        }
+    }
+    if (i == numConfigs) {
+        config = supportedConfigs[0];
+    }
+
+    if (config == nullptr) {
+        LOGE("Unable to initialize EGLConfig");
+        return -1;
+    }
+
+    /* EGL_NATIVE_VISUAL_ID is an attribute of the EGLConfig that is
+     * guaranteed to be accepted by ANativeWindow_setBuffersGeometry().
+     * As soon as we picked a EGLConfig, we can safely reconfigure the
+     * ANativeWindow buffers to match, using EGL_NATIVE_VISUAL_ID. */
+    eglGetConfigAttrib(eglDisplay, config, EGL_NATIVE_VISUAL_ID, &format);
+    surface = eglCreateWindowSurface(eglDisplay, config, engine->app->window, nullptr);
+    context = eglCreateContext(eglDisplay, config, nullptr, nullptr);
+
+    if (eglMakeCurrent(eglDisplay, surface, surface, context) == EGL_FALSE) {
+        LOGE("Unable to eglMakeCurrent");
+        return -1;
+    }
+
+    eglQuerySurface(eglDisplay, surface, EGL_WIDTH, &w);
+    eglQuerySurface(eglDisplay, surface, EGL_HEIGHT, &h);
+
+    engine->display = eglDisplay;
+    engine->context = context;
+    engine->surface = surface;
+    engine->width = w;
+    engine->height = h;
+    engine->state.angle = 0;
+
+    // Check openGL on the system
+    auto opengl_info = {GL_VENDOR, GL_RENDERER, GL_VERSION, GL_EXTENSIONS};
+    for (auto name : opengl_info) {
+        auto info = glGetString(name);
+        LOGD("OpenGL Info: %s", info);
+    }
+    // Initialize GL state.
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
+    glEnable(GL_CULL_FACE);
+    glShadeModel(GL_SMOOTH);
+    glDisable(GL_DEPTH_TEST);
+
+    return 0;
 }
+
